a9c13c2468929fea6515d3250ff698a3bcb2cedd
Benjaminlucky <42000766+Benjaminlucky@users.noreply.github.com>
2024-06-15 19:39:39 +0100

    Create and add Footer Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Signup from "./pages/Signup";
 import Projects from "./pages/Projects";
 import Dashboard from "./pages/Dashboard";
 import Header from "./components/Header";
+import FooterComp from "./components/Footer";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
+      <FooterComp />
     </BrowserRouter>
   );
 }
